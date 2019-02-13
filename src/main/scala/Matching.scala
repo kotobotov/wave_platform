475b0b43492501515b0f8f4fe777cc87023472ac
Kotobotov.ru <dima@kotobotov.ru>
2019-02-14 00:53:38 +0700

    :tada: Initial commit.

---
+++ src/main/scala/Matching.scala
@@ -0,0 +1,74 @@
+/**
+ * Created by Kotobotov.ru on 13.02.2019.
+ */
+object Matching extends App {
+  val ORDERS_LIMIT = 1000000.toDouble
+
+  val orderList ="""C8	b	C	15	4
+                   |C2	s	C	14	5
+                   |C2	s	C	13	2
+                   |C9	b	B	6	4
+                   |C4	b	D	5	4
+                   |C8	b	D	4	5
+                   |C8	b	A	11	1
+                   |C6	b	C	15	1""".stripMargin.split("\n").toList
+
+  val clients = """C1	1000	130	240	760	320
+                  |C2	4350	370	120	950	560
+                  |C3	2760	0	0	0	0
+                  |C4	560	450	540	480	950
+                  |C5	1500	0	0	400	100
+                  |C6	1300	890	320	100	0
+                  |C7	750	20	0	790	0
+                  |C8	7000	90	190	0	0
+                  |C9	7250	190	190	0	280""".stripMargin.split("\n").toList
+
+
+  lazy val buyRule = 0 // покупать только сколько есть денег (ордер остается но не выполняется)
+  lazy val sellRule = 0 // продавать только сколько есть бумаг
+
+  def buyOrder(o: Order) = o.price.toDouble + ((ORDERS_LIMIT-o.id) / ORDERS_LIMIT) // заявки обрабатываются согласно порядку, порядок мы учитываем вместе с ценой, количество учитываемых заявок ограничен точностью и размером данных, ограничение записанно в ORDERS_LIMIT чтоб не появлялось ошибок
+  def sellOrder(o: Order) = -o.price.toDouble - (o.id / ORDERS_LIMIT)
+
+  val sellQue = scala.collection.mutable.PriorityQueue.empty[Order](Ordering.by(sellOrder)) // используется "куча(пирамида)" для эффективной сортировки порядка заявок в стакане
+  val buyQue = scala.collection.mutable.PriorityQueue.empty[Order](Ordering.by(buyOrder)) // используется "куча(пирамида)" для эффективной сортировки порядка заявок в стакане
+
+  lazy val buyOrders = 0
+
+  val price = 12.9000000001 / 6
+  price.toInt
+
+  val dataStore = new FileIO with MokupData
+
+  while(!dataStore.isEmpty){
+    dataStore.next
+
+  sellQue.enqueue(
+    inputData:_*
+  )
+
+  buyQue.enqueue(
+    inputData:_*
+  )
+  }
+  val curOrder = sellQue.dequeue()
+
+
+
+
+
+
+  TradeSystem.obrabotat(curOrder)
+
+  case object TradeSystem{
+    def stakan(tiket:String) = Map("C" -> scala.collection.mutable.PriorityQueue[Order])
+    def obrabotat(order:Order) =
+
+  }
+//sellOrder(sellQue.dequeue())
+//sellOrder(sellQue.dequeue())
+//sellOrder(sellQue.dequeue())
+//sellOrder(sellQue.dequeue())
+
+  buyQue.dequeueAll
+}
---
+++ src/main/scala/Order.scala
@@ -0,0 +1,5 @@
+/**
+ * Created by Kotobotov.ru on 13.02.2019.
+ */
+case class Order(id:Int, client:String, direction:String, ticket:String, price:Int, amount:Int)
+
