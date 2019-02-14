aa43a2b963bdf4dce78fd636e5318a16cb2e4eee
Kotobotov.ru <dima@kotobotov.ru>
2019-02-15 00:30:14 +0700

    :boom: add Trading System

--- src/main/scala/Matching.scala
+++ src/main/scala/Matching.scala
@@ -1,33 +1,58 @@
 import config.Files
 import dataServices._
-import config.Files._
-import model.Order
+import model._
+
 /**
- * Created by Kotobotov.ru on 13.02.2019.
- */
+  * Created by Kotobotov.ru on 13.02.2019.
+  */
 object Matching extends App {
-  lazy val buyRule = 0 // покупать только столько сколько есть денег (ордер остается но не выполняется)
-  lazy val sellRule = 0 // продавать только столько сколько есть бумаг
-
-  def buyOrder(o: Order) = o.price.toDouble + ((Files.ORDERS_LIMIT-o.id) / Files.ORDERS_LIMIT) // заявки обрабатываются согласно порядку, порядок мы учитываем вместе с ценой, количество учитываемых заявок ограничен точностью и размером данных, ограничение записанно в ORDERS_LIMIT чтоб не появлялось ошибок
-  def sellOrder(o: Order) = -o.price.toDouble - (o.id / Files.ORDERS_LIMIT)
-
-  val sellQue = scala.collection.mutable.PriorityQueue.empty[Order](Ordering.by(sellOrder)) // используется "куча(пирамида)" для эффективной сортировки порядка заявок в стакане
-  val buyQue = scala.collection.mutable.PriorityQueue.empty[Order](Ordering.by(buyOrder))
+  lazy val buyRule = 0 // покупать  столько сколько есть денег (ордер остается но не выполняется)
+  lazy val sellRule = 0 // продавать  столько сколько есть бумаг
 
   val dataStore = new OrdersFromFile
 
-  while(!dataStore.isEmpty){
-    val currentOrder =  dataStore.next
+  while (!dataStore.isEmpty) { // использую проход по итератору с такой идей чтоб можно было обрабатывать гиганские файлы, не загружая логи в память)
+    val currentOrder = dataStore.next
     TradeSystem.execute(currentOrder)
-
   }
 
-    sellQue.dequeue()
+  case object TradeSystem {
+    def buyOrder(o: Order) =
+      o.price.toDouble + ((Files.ORDERS_LIMIT - o.id) / Files.ORDERS_LIMIT) // заявки обрабатываются согласно очередности добавления заявки и ее цене, есть ограничение по которому мы отслеживаием порядок, оно указанно в config.Files.ORDERS_LIMIT
+    def sellOrder(o: Order) = -o.price.toDouble - (o.id / Files.ORDERS_LIMIT)
 
-  case object TradeSystem{
+    // используется "куча(пирамида)" для эффективной сортировки порядка заявок в стакане
+    def CreateBuyingQue: scala.collection.mutable.PriorityQueue[Order] =
+      scala.collection.mutable.PriorityQueue.empty[Order](Ordering.by(buyOrder))
+    def CreateSellingQue: scala.collection.mutable.PriorityQueue[Order] =
+      scala.collection.mutable.PriorityQueue
+        .empty[Order](Ordering.by(sellOrder))
+    var clients = Array.empty[Client]
+    val market: Map[(String, Direction),
+                    scala.collection.mutable.PriorityQueue[Order]] = Map(
+      ("A", Buy) -> CreateBuyingQue,
+      ("B", Buy) -> CreateBuyingQue,
+      ("C", Buy) -> CreateBuyingQue,
+      ("D", Buy) -> CreateBuyingQue,
+      ("A", Sell) -> CreateSellingQue,
+      ("B", Sell) -> CreateSellingQue,
+      ("C", Sell) -> CreateSellingQue,
+      ("D", Sell) -> CreateSellingQue
+    )
+    def putToStakan(order: Order) = {
+      market(order.ticket, order.direction.reverse).dequeue()
+      market(order.ticket, order.direction) += order
+    }
+
+    def fromStakan(order: Order) = {}
+
+    def loadClients(clients: Array[Client]) = this.clients = clients
     //def stakan(tiket:String) = Map("C" -> scala.collection.mutable.PriorityQueue[Order])
-    def execute(order:Order) ={
+    def execute(order: Order) = {
+
+      order match {
+        case Order(_, clientId, direction, ticket, price, amount) =>
+      }
 
     }
 
--- src/main/scala/model/Direction.scala
+++ src/main/scala/model/Direction.scala
@@ -1,16 +1,25 @@
 package model
 
 /**
- * Created by Kotobotov.ru on 14.02.2019.
- */
-
-sealed trait Direction
-case object Buy extends Direction
-case object Sell extends Direction
+  * Created by Kotobotov.ru on 14.02.2019.
+  */
+sealed trait Direction {
+  def reverse:Direction
+}
+case object Buy extends Direction {
+  override def reverse: Direction = Sell
+}
+case object Sell extends Direction {
+  override def reverse: Direction = Buy
+}
 
 object Direction {
   def apply(input: String): Direction = input match {
     case "b" => Buy
-    case _ => Sell
+    case _   => Sell
+  }
+  def reverse(direction: Direction) = direction match {
+    case Buy => Sell
+    case _   => Buy
   }
 }
