4c18d2d73352a588e69d5368f192d6463124d561
Kotobotov.ru <dima@kotobotov.ru>
2019-02-15 20:36:13 +0700

    :wrench: update config

--- src/main/scala/config/Files.scala
+++ src/main/scala/config/Files.scala
@@ -1,11 +1,11 @@
 package config
 
 /**
- * Created by Kotobotov.ru on 14.02.2019.
- */
+  * Created by Kotobotov.ru on 14.02.2019.
+  */
 object Files {
-  val ORDERS_LIMIT = 1000000.toDouble
-val CLIENTS = "resource/clients.txt"
-val ORDERS = "resource/orders.txt"
-val RESULT = ""
+  val ORDERS_LIMIT = 1000000.toDouble // участвует в контроле очередности исполнения заявок с одинаковой ценой
+  val CLIENTS = "resource/clients.txt"
+  val ORDERS = "resource/orders.txt"
+  val RESULT = "resource/result.txt"
 }
